fa030058cc5a151ef0a4aa6dec44350d163b9467
e_virassamy <edwin.virassamy@infotel.com>
2023-07-24 14:26:34 +0200

    <feat> Endpoint getAllIssues without select

--- src/main/java/com/infotel/mantis_api/service/IssuesService.java
+++ src/main/java/com/infotel/mantis_api/service/IssuesService.java
@@ -1,15 +1,11 @@
 package com.infotel.mantis_api.service;
 
 import com.infotel.mantis_api.model.Issue;
-import com.infotel.mantis_api.model.IssueRecap;
 
 import java.util.List;
 
 public interface IssuesService {
     Issue searchIssue (int id);
-    List<Issue> searchAllIssues (int pageSize, int page, List<String> select, int projectId, String filterId);
 
-    List<IssueRecap> searchAllIssues();
-
-    List<IssueRecap> searchAllIssues(int pageSize, int page);
+    List<Issue> searchAllIssues(int pageSize, int page, List<String> selectValues);
 }
